c6619e113512ff10cdccc46f5e2f8d2cf823f2c5
Emmanuel Ezeka <eec.studies@gmail.com>
2020-08-13 00:00:28 +0100

    made some changes on the footer

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+let body = document.querySelector("body");
 let bio = document.querySelector(".bio");
-let share = document.querySelector(".share");
+let share = document.querySelector(".share-hide");
 let lady = document.querySelector(".image-name-date");
 let icon = document.querySelector(".icon"); 
+let pop = document.querySelector(".pop-up")
+let iconImage = document.querySelector(".fill");
 
 icon.addEventListener('click',()=>{
     bio.classList.toggle("dark");
-    share.classList.toggle("hidden");
+    share.classList.toggle("share-show");
     lady.classList.toggle("hidden");
+    pop.classList.toggle("pop-show")
+    iconImage.classList.toggle("filter")
+    icon.classList.toggle("dark-icon")
 })
+
